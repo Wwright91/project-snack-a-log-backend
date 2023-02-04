d07cabfebbb83e5c2c61a4674c0292f716d7d7df
Tech-Kat <katpelle@pursuit.org>
2023-02-03 20:25:30 -0500

    pizza info

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,7 @@
 \c snack_a_log; 
 
 INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES
+('Pizza', 3, 12, 4, false, 'https://media.gettyimages.com/id/769806947/photo/close-up-of-pizza-in-plate-on-table.jpg?s=612x612&w=0&k=20&c=xnnR6_whoVZZHikHqhzrdR6MiYis7rPZazkiizCN_M4='),
 ('Strawberries', 20, 10, 0, true, 'https://picsum.photos/id/1080/300/300'),
 ('Raspberries', 16, 4, 0, true, 'https://picsum.photos/id/102/300/300'),
 ('Honey Covered Granola',  30, 12, 22, false, 'https://picsum.photos/id/312/300/300'),
